e5a0a1543350b0118dce6fda4ab8d2e201a7f005
hareeshramineni <hareeshramineni@gmail.com>
2026-02-02 18:58:40 +0530

    different ways of writting the JSX

--- App.js
+++ App.js
@@ -2,25 +2,34 @@
 import React from "react";
 import ReactDOM from "react-dom/client"
 
-//React.createElemnt => javascript object=> when we Render this to DOM => Html Element
-const heading = React.createElement("h1",{id:"heading"},"Namaste React🚀");
-console.log(heading);
 
-//JSX => is not HTML =>it is HTML or xml like Syntax
-//JSX (transpiled before it reaches the JS Engine)-PARCEL-Babel
-//JSX => Babel Transpile it to React.createElement => ReactElement => JS Object =>Html Element(render)
-const jsxHeading = <h1 className="heading">Namste React Using JSX 🚀</h1>;
+// React Element
+const heading = (
+<h1 className="heading">
+  Namste React Using JSX 🚀
+</h1>);
 
-const jsxHeadingMultiLine = (<h1 id="heading">
-  New Namste React Using JSX 🚀
-  </h1>);
+// React Component
+// class Based Component - old
+//Functional Coponent - new
 
-console.log(jsxHeading);
+// React Functional Component - just a normal javaScript Function => which returns some JSX
+
+const HeadingComponent = ()=><h1 className>Namste React Functional Component</h1>;
+
+const HeadingComponentNew = ()=>(
+<h1 className="header">Namste React Functional Component</h1>
+);
+
+const HeadingComponentNewer = ()=>{
+  return(
+<h1 className="header">Namste React Functional Component</h1>
+)};
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
 root.render(heading);
-root.render(jsxHeading);
-root.render(jsxHeadingMultiLine);
+root.render(<HeadingComponent/>);
+
 
 
