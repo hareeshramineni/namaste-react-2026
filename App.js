ea21e9806629e8712a066983ab57a96168fb95f7
hareeshramineni <hareeshramineni@gmail.com>
2026-02-02 19:11:37 +0530

    understanding component composistion

--- App.js
+++ App.js
@@ -3,33 +3,25 @@ import React from "react";
 import ReactDOM from "react-dom/client"
 
 
-// React Element
-const heading = (
-<h1 className="heading">
-  Namste React Using JSX 🚀
-</h1>);
+const Title = ()=>(
+  <h1 className="head" tabIndex="5">
+    React Using JSX 🚀
+    </h1>
+)
 
-// React Component
-// class Based Component - old
-//Functional Coponent - new
-
-// React Functional Component - just a normal javaScript Function => which returns some JSX
-
-const HeadingComponent = ()=><h1 className>Namste React Functional Component</h1>;
-
-const HeadingComponentNew = ()=>(
-<h1 className="header">Namste React Functional Component</h1>
-);
-
-const HeadingComponentNewer = ()=>{
+const HeadingComponent = ()=>{
   return(
+    <div id="container">
+    {/* component Composistion */}
+      <Title/>
 <h1 className="header">Namste React Functional Component</h1>
+</div>
 )};
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
-root.render(heading);
 root.render(<HeadingComponent/>);
+
 
 
 
