ee99b8f1000cc3bdc14f4f71db2c82cc6f4dd23b
hareeshramineni <hareeshramineni@gmail.com>
2026-02-02 12:15:38 +0530

    episode-01

---
+++ App.js
@@ -0,0 +1,24 @@
+  
+  const parent= React.createElement("div",{id:"parent"},
+   [
+     React.createElement("div",{id:"chid"},
+      [React.createElement("h1",{},"I'm an h1 tag"),
+        React.createElement("h2",{},"I'm an h2 tag")
+      ]
+    ),
+
+     React.createElement("div",{id:"chid2"},
+      [React.createElement("h1",{},"I'm an h1 tag"),
+        React.createElement("h2",{},"I'm an h2 tag")
+      ]
+    )
+
+   ]
+  );
+  
+  const heading =React.createElement("h1",{id:"heading",xyz:"abc"},"Hello world from React");
+
+  console.log(heading);
+
+  const root  = ReactDOM.createRoot(document.getElementById("root"));
+  root.render(parent);
